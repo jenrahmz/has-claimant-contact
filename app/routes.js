f4fe88d95bbb002860931a1e3e2ddae829a02d5d
Colin <colin.oakley@engineering.digital.dwp.gov.uk>
2022-10-04 09:19:21 +0100

    adding validation example

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,33 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+
+function checkEnterFieldsErrors(data) {
+    console.log(data);
+    const error = {}
+    if (data['mobile-number'] === '') {
+        error['mobile-number'] = {
+           text: 'Enter mobile number',
+           href: '#mobile-number'
+        };
+    }
+    return error;
+}
+
+router.post('/version-0-1/select-template', (req, res) => {
+    res.redirect('/version-0-1/enter-fields');
+});
+
+router.post('/version-0-1/enter-fields', (req, res) => {
+
+    const error = checkEnterFieldsErrors(req.body);
+    if(Object.keys(error).length === 0) {
+        res.redirect('/version-0-1/check-answers');
+    } else {
+        res.render('version-0-1/enter-fields.html', { error, errorlist: Object.values(error) });
+    }
+});
+
+
+
 module.exports = router
